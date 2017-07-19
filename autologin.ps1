8b68445a0b4f412825f32f2f4d427f3e06f45dbc
ankita0210 <ankita.shrivastava@cognizant.com>
2017-07-19 18:07:58 +0530

    Add files via upload

---
+++ autologin.ps1
@@ -0,0 +1,19 @@
+﻿
+##Use this creds for multi-subs scenario
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+
+$password = ConvertTo-SecureString "CJtbj9QFDI9RHSk3AccyL3BX8XC3WLYwEjryeVHJYzY=" -AsPlainText –Force
+$AppId = "126e07a8-75c1-477e-9f85-b343e35e214d"
+$tenantId = "188285f7-8f1e-4c0d-a0bc-797e3e38c5b3"
+
+$cred = New-Object -TypeName pscredential –ArgumentList $AppId , $password
+
+Login-AzureRmAccount -Credential $cred -ServicePrincipal –TenantId $tenantId
+
+
+
+  
+
+
+
+ 
